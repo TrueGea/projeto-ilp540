08a70354f91f6f64502aee2817bdc668731fe898
TrueGea <100365780+TrueGea@users.noreply.github.com>
2022-03-03 12:31:29 -0300

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=h1, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="bootstrap/bootstrap.min.css">
+
+</head>
+<body>
+
+    <header class="topo">
+         
+
+    </header>
+    <main>
+
+    </main>
+    <footer>
+
+    </footer>
+    <script src="js/jquery-3.3.1.slim.min.js" ></script>
+<script src="js/popper.min.js" ></script>
+<script src="js/bootstrap.min.js"></script>
+
+</body>
+</html>
