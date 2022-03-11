d37d809ada639fa2e7c99933178162bf276f83b3
TrueGea <100365780+TrueGea@users.noreply.github.com>
2022-03-10 21:12:42 -0300

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=h1, initial-scale=1.0">
+        <title>Biblioteca LerNuvens</title>
+        <link rel="stylesheet" href="bootstrap/bootstrap.min.css">
+        <link rel="stylesheet" href="index.css">
+    
+    </head>
+    <body class="backimag">
+        <header>
+            <nav>
+                <ul>
+                    <li><a class="active" href="#home">Home</a></li>
+                    <li><a href="consultas">Cadastro</a></li>
+                    <li><a href="#exames">Emprestar Livros</a></li>
+                    <li><a href="#vacinas">Devoluções de Livros</a></li>
+                    <li><a href="#cirurgias">Sugestoes de Livros</a></li>
+                    <li></li>
+                </ul>
+        
+            </nav>
+        </header>
+        <main>
+            <div class="box">              
+                <h2 class="format">Sobre Nos!</h2>
+            </div>
+        </main>
+           
+        <script src="js/jquery-3.3.1.slim.min.js" ></script>
+        <script src="js/popper.min.js" ></script>
+        <script src="js/bootstrap.min.js"></script>
+    
+    </body>
+</html>
