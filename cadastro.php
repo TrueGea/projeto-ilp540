e65756d1c3f174ebb6625c1e77c11b2336d66104
TrueGea <100365780+TrueGea@users.noreply.github.com>
2022-04-07 21:51:42 -0300

    Update cadastro.php

--- cadastro.php
+++ cadastro.php
@@ -6,7 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="bootstrap/bootstrap.min.css">
     <link rel="stylesheet" href="cadastro.css">
-    <script src="script.js"></script>
     <title>Cadastro</title>
 </head>
 <body>
